0374f7c9f56655dea6bc1b35db3ff039923161fc
Fnoberz <SarnaxOfficial@protonmail.com>
2022-11-08 03:52:41 +0700

    Update Windows Priority game tweak.bat

--- 06 - Priority Game/Windows Priority game tweak.bat
+++ 06 - Priority Game/Windows Priority game tweak.bat
@@ -7,3 +7,5 @@ wmic process where name="GTA5.exe" CALL setpriority "high priority"
 wmic process where name="gtavlauncher.exe" CALL setpriority "idle"
 
 wmic process where name="subprocess.exe" CALL setpriority "idle"
+
+exit
